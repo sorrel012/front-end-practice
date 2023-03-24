d6ba4670e39f2bdb845ff0b2036b775c1dd422c9
hwh <sorrel012@gmail.com>
2023-03-25 00:00:30 +0900

    feat: create Object that has same function with Math

--- javascript/object_oriented_programming/built-in.js
+++ javascript/object_oriented_programming/built-in.js
@@ -1,3 +1,22 @@
 console.log('Math.PI:', Math.PI);
 console.log('Math.random():', Math.random());
 console.log('Math.floor(3.9):', Math.floor(3.9));
+
+console.log();
+
+// Math 객체와 동일한 기능을 하는 객체 만들기
+
+ var MyMath = {
+    PI:3.141592653589793,
+    random:function() {
+        return Math.random(); //random 활용..
+    },
+    floor:function(val) {
+        return Math.floor(val);
+    }
+ }
+
+ console.log('MyMath.PI:', MyMath.PI);
+ console.log('MyMath.random():', MyMath.random());
+ console.log('MyMath.floor(5.8):', MyMath.floor(5.8));
+ 
