5ec43bcd670000fd62bccd9e126581afff7675ec
sorrel012 <wain1719@gmail.com>
2023-11-14 15:01:28 +0900

    feat: Explore javascript else-if statement

--- javascript/basic/if.js
+++ javascript/basic/if.js
@@ -1,8 +1,17 @@
-console.log("BEFORE CONDITIONAL");
-let random = Math.random();
-if(random < 0.5) {
-  console.log(random, "YOUR NUMBER IS LESS THAN 0.5!");
- } else {
-  console.log(random, "YOUR NUMBER IS 0.5 OR GREATER!");
- }
-console.log("AFTER CONDITIONAL");
+// console.log("BEFORE CONDITIONAL");
+// let random = Math.random();
+// if(random < 0.5) {
+//   console.log(random, "YOUR NUMBER IS LESS THAN 0.5!");
+//  } else {
+//   console.log(random, "YOUR NUMBER IS 0.5 OR GREATER!");
+//  }
+// console.log("AFTER CONDITIONAL");
+
+const dayOfWeek = 'Monday';
+if(dayOfWeek === 'Monday') {
+  console.log('UGHHH I HATE MONDAYS!');
+} else if (dayOfWeek === 'Saturday') {
+  console.log('YAY I LOVE SATURDAYS');
+} else if (dayOfWeek === 'Friday') {
+  console.log('FRIDAYS ARE DECENT, ESCEPCIALLY AFTER WORK!');
+}
