0fc968aae6e908aea55aa6cdafe5a915bb5198ca
sorrel012 <wain1719@gmail.com>
2023-11-14 14:50:09 +0900

    feat: Explore javascript if statement

---
+++ javascript/basic/app.js
@@ -0,0 +1,3 @@
+console.log("HELLO~");
+let total = 1 + 3;
+console.log("GoodBye");
---
+++ javascript/basic/if.js
@@ -0,0 +1,8 @@
+console.log("BEFORE CONDITIONAL");
+let random = Math.random();
+if(random < 0.5) {
+  console.log(random, "YOUR NUMBER IS LESS THAN 0.5!");
+ } else {
+  console.log(random, "YOUR NUMBER IS 0.5 OR GREATER!");
+ }
+console.log("AFTER CONDITIONAL");
