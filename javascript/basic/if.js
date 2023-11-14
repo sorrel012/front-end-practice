1ce25ba4a324a052c131e7e9630b063faa3f6212
sorrel012 <wain1719@gmail.com>
2023-11-14 15:20:51 +0900

    feat: Explore javascript else statement

--- javascript/basic/if.js
+++ javascript/basic/if.js
@@ -3,15 +3,17 @@
 // if(random < 0.5) {
 //   console.log(random, "YOUR NUMBER IS LESS THAN 0.5!");
 //  } else {
-//   console.log(random, "YOUR NUMBER IS 0.5 OR GREATER!");
+//   console.log(random, "YOUR NUMBER IS GREATER (OR EQUAL) THAN 0.5!");
 //  }
 // console.log("AFTER CONDITIONAL");
 
-const dayOfWeek = 'Monday';
-if(dayOfWeek === 'Monday') {
+const dayOfWeek = prompt('ENTER A DAY').toLowerCase();
+if(dayOfWeek === 'monday') {
   console.log('UGHHH I HATE MONDAYS!');
-} else if (dayOfWeek === 'Saturday') {
+} else if (dayOfWeek === 'saturday') {
   console.log('YAY I LOVE SATURDAYS');
-} else if (dayOfWeek === 'Friday') {
+} else if (dayOfWeek === 'friday') {
   console.log('FRIDAYS ARE DECENT, ESCEPCIALLY AFTER WORK!');
+} else {
+  console.log('MEH');
 }
