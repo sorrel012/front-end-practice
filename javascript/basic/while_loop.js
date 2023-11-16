250892f492711199018097bd493131399c146396
sorrel012 <wain1719@gmail.com>
2023-11-16 15:10:46 +0900

    feat: Explore javascript while loop

--- javascript/basic/while_loop.js
+++ javascript/basic/while_loop.js
@@ -3,9 +3,18 @@
 //   console.log(++count);
 // }
 
-const SECRET = 'BabyHippo';
-let guess = prompt('enter the secret code...');
-while ( guess !== SECRET) {
-  guess = prompt('enter the secret code...');
+// const SECRET = 'BabyHippo';
+// let guess = prompt('enter the secret code...');
+// while ( guess !== SECRET) {
+//   guess = prompt('enter the secret code...');
+// }
+// console.log("CONGRATS YOU GOT THE SECRET!!!");
+
+let input = prompt('Hey, say something!');
+while (true) {
+  input = prompt(input);
+  if (input.toLocaleLowerCase() === 'stop copying me') {
+    break;
+  }
 }
-console.log("CONGRATS YOU GOT THE SECRET!!!");
+console.log('OK YOU WIN!');
