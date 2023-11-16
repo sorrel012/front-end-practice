cd6a714a91c1b708245ff7b199df250537dae1bc
sorrel012 <wain1719@gmail.com>
2023-11-16 11:11:28 +0900

    feat: Explore javascript infinite loop

--- javascript/basic/loop.js
+++ javascript/basic/loop.js
@@ -1,3 +1,11 @@
-for (let i = 1; i <= 10; i++) {
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for(let i = 0; i < 21; i+= 2){
+//   console.log(i);
+// }
+
+for (let i = 0; i > 0; i++) {
   console.log(i);
 }
