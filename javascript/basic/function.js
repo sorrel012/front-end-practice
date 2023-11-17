1ad739bef11245cedf69e89eca0c12bd9d1d7942
sorrel012 <wain1719@gmail.com>
2023-11-17 15:41:02 +0900

    feat: Explore javascript function

---
+++ javascript/basic/function.js
@@ -0,0 +1,15 @@
+function sing() {
+  console.log('Do');
+  console.log('RE');
+  console.log('MI');
+}
+
+// sing();
+// sing();
+// sing();
+
+function greet(frstName) {
+  console.log(`FirstName is: ${frstName}`);
+}
+
+greet('Hana');
