2e1c4d07f2336979e747e3b90ec245c740e42caa
sorrel012 <wain1719@gmail.com>
2023-11-17 17:17:52 +0900

    feat: Explore javascript try-catch

---
+++ javascript/basic/try-catch.js
@@ -0,0 +1,16 @@
+// try {
+//   hello.toUpperCase();
+// } catch {
+//   console.log('ERROR!'); 
+// }
+
+// console.log('AFTER ERROR');
+
+function yell(msg) {
+  try {
+    console.log(msg.toUpperCase().repeat(3));
+  } catch(e) {
+    console.log(e);
+    console.log('Please pass a string next time!');
+  }
+}
