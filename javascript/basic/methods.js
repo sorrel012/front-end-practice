de915183719128793009fe3b44a52b9fb45fa1ad
sorrel012 <wain1719@gmail.com>
2023-11-17 17:09:48 +0900

    feat: Explore javascript this

--- javascript/basic/methods.js
+++ javascript/basic/methods.js
@@ -7,3 +7,14 @@ const myMath = {
     return num ** 3;
   }
 }
+
+const cat = {
+  name: 'Blue',
+  color: 'grey',
+  meow() {
+    console.log(`THIS IS : ${this}`);
+    console.log(`${this.name} says MEOWWW`);
+  }
+}
+
+const meow2 = cat.meow;
