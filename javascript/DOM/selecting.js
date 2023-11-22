a55389fdf07276081db76f2e36263c54916beb8b
sorrel012 <wain1719@gmail.com>
2023-11-22 10:47:18 +0900

    feat: Explore javascript selector

---
+++ javascript/DOM/selecting.js
@@ -0,0 +1,17 @@
+// const allImages = document.getElementsByTagName('img');
+//
+// for (let img of allImages) {
+//   console.log(img.src);
+// }
+
+// const squreImages = document.getElementsByClassName('square');
+//
+// for (let image of squreImages) {
+//   console.log(img.src);
+// }
+
+const links = document.querySelectorAll('p a');
+
+for (let link of links) {
+  console.log(link.href);
+}
