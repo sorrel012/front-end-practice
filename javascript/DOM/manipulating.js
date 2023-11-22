c0616dbd02594534e34e495c4eace3cf88c640d0
sorrel012 <wain1719@gmail.com>
2023-11-22 11:01:48 +0900

    feat: Manipulate javascript attributes

--- javascript/DOM/manipulating.js
+++ javascript/DOM/manipulating.js
@@ -1,8 +1,16 @@
 const allLinks = document.querySelectorAll('a');
 
-for (let link of allLinks) {
-  link.innerText = 'I AM A LINK!!!!';
-}
+// for (let link of allLinks) {
+//   link.innerText = 'I AM A LINK!!!!';
+// }
 
 document.querySelector('h1').innerText += ' version';
 document.querySelector('h1').innerHTML += '<sup>2</sup>';
+
+const firstLink = document.querySelector('a');
+const link1 = firstLink.href;
+const link2 = firstLink.getAttribute('href');
+console.log(link1);
+console.log(link2);
+
+firstLink.setAttribute('href', 'https://www.google.com');
