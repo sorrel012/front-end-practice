4aab92035b2b87626f4384ff8c83904c5af22200
sorrel012 <wain1719@gmail.com>
2023-11-22 15:06:11 +0900

    feat: Practice javascript DOM

---
+++ javascript/DOM/pokemon.js
@@ -0,0 +1,15 @@
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('#container');
+
+for (let i = 1; i <= 1017; i++) {
+  const pokemon = document.createElement('div');
+  pokemon.classList.add('pokemon');
+  const label = document.createElement('span');
+  label.innerText = `#${i}`;
+  const newImg = document.createElement('img');
+  newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+  pokemon.appendChild(newImg);
+  pokemon.appendChild(label);
+  container.appendChild(pokemon);
+}
