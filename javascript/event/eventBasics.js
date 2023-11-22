32fdc31a4ce7f37407a4fffaf72f27a97b31dccb
sorrel012 <wain1719@gmail.com>
2023-11-22 17:11:30 +0900

    feat: Explore javascript addEventListener

--- javascript/event/eventBasics.js
+++ javascript/event/eventBasics.js
@@ -11,3 +11,28 @@ function scream() {
 }
 
 btn.onmouseenter = scream;
+
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('dblclick', function () {
+  alert('DOUBLE CLICKED');
+});
+
+// btn3.addEventListener('mouseup', function () {
+//   alert('MOUSE UP');
+// });
+
+function twist() {
+  console.log('TWIST!');
+}
+function shout() {
+  console.log('SHOUT!');
+}
+
+const tasButton = document.querySelector('#tas');
+
+// 가장 나중에 선언된 이벤트만 적용됨.(여러 개 불가!)
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+tasButton.addEventListener('click', twist, { once: true });
+tasButton.addEventListener('click', shout);
