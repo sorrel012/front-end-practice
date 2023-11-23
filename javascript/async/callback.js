36a5bbef4a27d6287402ec6b82b173fe7220357a
sorrel012 <wain1719@gmail.com>
2023-11-23 17:44:05 +0900

    feat: Explore javascript callback

---
+++ javascript/async/callback.js
@@ -0,0 +1,41 @@
+// setTimeout(() => {
+//   document.body.style.backgroundColor = 'red';
+//   setTimeout(() => {
+//     document.body.style.backgroundColor = 'orange';
+//   }, 1000);
+// }, 1000);
+
+const delayedColorChange = (newColor, delay, doNext) => {
+  setTimeout(() => {
+    document.body.style.backgroundColor = newColor;
+    doNext && doNext();
+  }, delay);
+};
+
+delayedColorChange('red', 1000, () => {
+  delayedColorChange('orange', 1000, () => {
+    delayedColorChange('yellow', 1000, () => {
+      delayedColorChange('green', 1000, () => {
+        delayedColorChange('blue', 1000);
+      });
+    });
+  });
+});
+
+searchMoviesAPI(
+  'amadeus',
+  () => {
+    saveToMyDB(
+      movies,
+      () => {
+        //if it works, run this
+      },
+      () => {
+        //if it doesn't work, run this
+      },
+    );
+  },
+  () => {
+    //if API is down, or request failed
+  },
+);
