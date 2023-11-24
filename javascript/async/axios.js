511133d3906df65b8c08838a82e4571cbdef44ef
sorrel012 <wain1719@gmail.com>
2023-11-24 18:00:17 +0900

    feat: Explore ajax api axios

---
+++ javascript/async/axios.js
@@ -0,0 +1,17 @@
+// axios
+//   .get('https://swapi.dev/api/people/1/')
+//   .then((res) => {
+//     console.log('RESPONSE: ', res);
+//   })
+//   .catch((error) => {
+//     console.log('ERROR: ', error);
+//   });
+
+const getStarWarsPerson = async () => {
+  try {
+    const res = await axios.get('https://swapi.dev/api/people/1/');
+    console.log(res.data);
+  } catch (e) {
+    console.log('ERROR', e);
+  }
+};
