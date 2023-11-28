563f90175cb4a2b3a3ec48dbd1a28a8cdbdedcde
sorrel012 <wain1719@gmail.com>
2023-11-28 17:48:27 +0900

    feat: Explore javascript constructor function

--- javascript/object_oriented_programming/oop.js
+++ javascript/object_oriented_programming/oop.js
@@ -25,3 +25,27 @@ function makeColor(r, g, b) {
 }
 
 const firstColor = makeColor(35, 255, 150);
+firstColor.hex();
+firstColor.rgb();
+
+const black = makeColor(0, 0, 0);
+black.rgb();
+black.hex();
+
+function Color(r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
+}
+
+Color.prototype.rgb = function () {
+  const { r, g, b } = this;
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+Color.prototype.hex = function () {
+  const { r, g, b } = this;
+  return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+const color1 = new Color(255, 40, 100);
