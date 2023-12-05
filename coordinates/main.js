f69918f29978a871862b3e42966ded6c13c9c455
sorrel012 <wain1719@gmail.com>
2023-12-05 17:00:46 +0900

    feat: Implement targeting program using window size and window coordinate

---
+++ coordinates/main.js
@@ -0,0 +1,17 @@
+const vertical = document.querySelector('.vertical');
+const horizontal = document.querySelector('.horizontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+document.addEventListener('mousemove', (evt) => {
+  const x = evt.clientX;
+  const y = evt.clientY;
+
+  vertical.style.left = `${x}px`;
+  horizontal.style.top = `${y}px`;
+  target.style.left = `${x}px`;
+  target.style.top = `${y}px`;
+  tag.style.left = `${x}px`;
+  tag.style.top = `${y}px`;
+  tag.innerText = `${x}px ${y}px`;
+});
