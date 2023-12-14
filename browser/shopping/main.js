3ed7b3741656607f674d4fd2c963980227005c1c
sorrel012 <wain1719@gmail.com>
2023-12-14 16:18:40 +0900

    refactor: Refactor Shopping list using evt

--- browser/shopping/main.js
+++ browser/shopping/main.js
@@ -21,50 +21,68 @@ function onAdd() {
   input.focus();
 }
 
-addBtn.addEventListener('click', () => {
-  onAdd();
-});
-
+let id = 0; // 실제로는 UUID 쓰는 것이 좋음
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.classList.add('item__row');
-
-  const item = document.createElement('div');
-  item.classList.add('item');
-
-  const itemName = document.createElement('span');
-  itemName.classList.add('item__name');
-  itemName.innerText = text;
-
-  const delBtn = document.createElement('button');
-  delBtn.classList.add('item__delete');
-  delBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  });
-
-  const icon = document.createElement('i');
-  icon.classList.add('fas');
-  icon.classList.add('fa-trash-alt');
-
-  const itemDivider = document.createElement('div');
-  itemDivider.classList.add('item__divider');
-
-  item.appendChild(itemName);
-  delBtn.appendChild(icon);
-  item.appendChild(delBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `  
+          <div class="item">
+            <span class="item__name">${text}</span>
+            <button class="item__delete"'>
+              <i class="fas fa-trash-alt" data-id='${id}'></i>
+            </button>
+          </div>
+          <div class="item__divider"></div>`;
+  id++;
+  // const item = document.createElement('div');
+  // item.classList.add('item');
+  //
+  // const itemName = document.createElement('span');
+  // itemName.classList.add('item__name');
+  // itemName.innerText = text;
+  //
+  // const delBtn = document.createElement('button');
+  // delBtn.classList.add('item__delete');
+  // delBtn.addEventListener('click', () => {
+  //   items.removeChild(itemRow);
+  // });
+  //
+  // const icon = document.createElement('i');
+  // icon.classList.add('fas');
+  // icon.classList.add('fa-trash-alt');
+  //
+  // const itemDivider = document.createElement('div');
+  // itemDivider.classList.add('item__divider');
+  //
+  // item.appendChild(itemName);
+  // delBtn.appendChild(icon);
+  // item.appendChild(delBtn);
+  //
+  // itemRow.appendChild(item);
+  // itemRow.appendChild(itemDivider);
 
   return itemRow;
 }
 
-delBtn.addEventListener('click', () => {
-  items.removeChild(itemRow);
+addBtn.addEventListener('click', () => {
+  onAdd();
 });
+
+// delBtn.addEventListener('click', () => {
+//   items.removeChild(itemRow);
+// });
 
 input.addEventListener('keypress', (evt) => {
   if (evt.key === 'Enter') {
     onAdd();
+  }
+});
+
+items.addEventListener('click', (evt) => {
+  const id = evt.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
