1f4038e35e443126d3f4bbaacb787394d1ff6980
sorrel012 <wain1719@gmail.com>
2023-12-18 14:47:43 +0900

    fix: Bind class using this

--- browser/carrot/src/field.js
+++ browser/carrot/src/field.js
@@ -41,7 +41,7 @@ export default class Field {
     }
   }
 
-  onClick(evt) {
+  onClick = (evt) => {
     const target = evt.target;
     if (target.matches('.carrot')) {
       target.remove();
@@ -50,7 +50,7 @@ export default class Field {
     } else if (target.matches('.bug')) {
       this.onItemClick && this.onItemClick('bug');
     }
-  }
+  };
 }
 
 function randomNumber(min, max) {
