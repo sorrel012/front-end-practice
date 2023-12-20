1ebdaa50debdd83729281163eb723579e98f7010
sorrel012 <wain1719@gmail.com>
2023-12-20 14:10:06 +0900

    refactor: Ensure data types

--- browser/carrot/src/game.js
+++ browser/carrot/src/game.js
@@ -42,7 +42,7 @@ class Game {
     this.gameBtn = document.querySelector('.game__button');
     this.gameBtn.addEventListener('click', () => {
       if (this.started) {
-        this.stop();
+        this.stop(Reason.cancel);
       } else {
         this.start();
       }
@@ -69,28 +69,14 @@ class Game {
     sound.playBackground();
   }
 
-  stop() {
+  stop(reason) {
     this.started = false;
     this.stopGameTimer();
     this.hideGameButton();
-    sound.playAlert();
     sound.StopBackground();
-    this.onGameStop && this.onGameStop(Reason.cancel);
+    this.onGameStop && this.onGameStop(reason);
   }
 
-  finish(win) {
-    this.started = false;
-    this.hideGameButton();
-    sound.StopBackground();
-    if (win) {
-      sound.playWin();
-    } else {
-      sound.playBug();
-    }
-    this.stopGameTimer();
-    sound.StopBackground();
-    this.onGameStop && this.onGameStop(win ? 'win' : 'lose');
-  }
   onItemClick = (item) => {
     if (!this.started) {
       return;
@@ -99,10 +85,10 @@ class Game {
       this.score++;
       this.updateScoreBoard();
       if (this.score === this.carrotCount) {
-        this.finish(true);
+        this.stop(Reason.win);
       }
     } else if (item === 'bug') {
-      this.finish(false);
+      this.stop(Reason.lose);
     }
   };
 
@@ -128,7 +114,7 @@ class Game {
     this.timer = setInterval(() => {
       if (remainingTimeSec <= 0) {
         clearInterval(timer);
-        this.finish(this.carrotCount === this.score);
+        this.finish(this.carrotCount === this.score ? Reason.win : Reason.lose);
         return;
       }
       this.updateTimerText(--remainingTimeSec);
--- browser/carrot/src/main.js
+++ browser/carrot/src/main.js
@@ -2,6 +2,7 @@
 
 import PopUp from './popup.js';
 import { GameBuilder, Reason } from './game.js';
+import * as sound from './sound';
 
 const CARROT_COUNT = 10;
 const BUG_COUNT = 10;
@@ -15,12 +16,15 @@ game.setGameStopListener((reason) => {
   switch (reason) {
     case Reason.cancel:
       message = 'Replay❔';
+      sound.playAlert();
       break;
     case Reason.win:
       message = 'YOU WON🎉';
+      sound.playWin();
       break;
     case Reason.lose:
       message = 'YOU LOST🚫';
+      sound.playBug();
       break;
     default:
       throw new Error('not valid reason');
